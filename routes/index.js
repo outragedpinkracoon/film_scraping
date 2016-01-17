75f69070a6040b6d0351a06020c46a1265da44f5
Valerie Gibson <vgibson14@gmail.com>
2016-01-17 17:34:32 +0000

    some tidying up

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-var FileUtils = require('custom/fileUtils');
+var FileService = require('../lib/services/fileService'); 
 
 var router = express.Router();
 
@@ -17,7 +17,7 @@ router.get('/', function (req, res, next) {
     var data = children.data;
     var week1sw = data, week1avatar = data;
     var json = { week1sw: week1sw, week1avatar: week1avatar };
-    res.render('index', json);
+    res.render('home/index', json);
 });
 
 
@@ -25,8 +25,8 @@ router.get('/scrape', function (req, res, next) {
     var url = 'http://www.boxofficemojo.com/showdowns/chart/?view=weekly&id=liberge.htm';
     request(url, function (error, response, html) {
         if (!error) {
-            new FileUtils().writeFile("result.html", html, function(result){
-                res.render('scrape', { result: result });
+            new FileService().writeFile("result.html", html, function(result){
+                res.render('scraping/index', { result: result });
             })
         }
     });
