a299711eb7ec07fd017e686c3e075664c4b18127
Valerie Gibson <vgibson14@gmail.com>
2016-01-17 18:21:04 +0000

    good grief

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,19 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     var $ = cheerio.load(fs.readFileSync('result.html'));
     var table = $("table")[2];
-    var tr = $(table).find("tr")[1];
-    var td = $(tr).find("td")[1];
-    var font1 = $(td).find("font")
-    var font2 = font1.find("font")[0]
-    var children = font2.children[0]
-    var data = children.data;
+    // var tr = $(table).find("tr")[1];
+    // var td = $(tr).find("td")[1];
+    // var font1 = $(td).find("font")
+    // var font2 = font1.find("font")[0]
+    // var children = font2.children[0]
+    // var data = children.data;
+    
+   var tr = $(table).children("tr")[0];
+   var td = $(tr).children("td")[1];
+   var a = $(td).find("a")[0];
+   
+    var child = a.children[2]
+    var data = child.data;
     var week1sw = data, week1avatar = data;
     var json = { week1sw: week1sw, week1avatar: week1avatar };
     res.render('home/index', json);
