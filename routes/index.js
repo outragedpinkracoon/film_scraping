6985e5029e40ba0fd37553264345158b4587ddaa
Valerie Gibson <vgibson14@gmail.com>
2016-01-24 15:34:07 +0000

    better

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,10 @@ var KeyValuePair = require('../lib/models/keyValuePair');
 
 var router = express.Router();
 
-var DataThing = function () {
+var Parser = function () {
     var $ = cheerio.load(fs.readFileSync('result.html'));
     this.init = function () {
-        this.headers();
+        this.results = this.headers();
         this.data();
     }
     this.headers = function () {
@@ -25,45 +25,29 @@ var DataThing = function () {
         });
         return results;
     },
-    this.data = function(){
+    this.data = function () {
         var elements = $("table").eq(2).find("tr").eq(2).find("td");
 
-    for (var i = 2; i < 6; i++) {
-        elements = $("table").eq(2).find("tr").eq(i).find("td");
-        for (var k = 0; k < 5; k++) {
-            var amount = elements.eq(k + 1).find("font").eq(1).text();
-            var kvp = new KeyValuePair();
-            kvp.value = amount;
-            kvp.key = k;
-            this.results[k].dataPoints.push(kvp);
+        for (var i = 2; i < 6; i++) {
+            elements = $("table").eq(2).find("tr").eq(i).find("td");
+            for (var k = 0; k < 5; k++) {
+                var amount = elements.eq(k + 1).find("font").eq(1).text();
+                var kvp = new KeyValuePair();
+                kvp.value = amount;
+                kvp.key = k;
+                this.results[k].dataPoints.push(kvp);
+            }
         }
     }
-    }
-    this.results = this.headers();
+    this.init(); 
 }
 
 router.get('/', function (req, res, next) {
 
-    var d = new DataThing();
-    d.init();
+    var parser = new Parser();
 
-    var json = { data: d.results };
+    var json = { data: parser.results };
     res.render('home/index', json);
 });
 
 module.exports = router;
-
-//find number
-    //var table = $("table")[2];
-    // var tr = $(table).find("tr")[1];
-    // var td = $(tr).find("td")[1];
-    // var font1 = $(td).find("font")
-    // var font2 = font1.find("font")[0]
-    // var children = font2.children[0]
-    // var data = children.data;
-    
-//find name
-    // var tr = $(table).children("tr")[0];
-    // var td = $(tr).children("td")[1];
-    // var a = $(td).find("a")[0];
-    // var child = a.children[2]
